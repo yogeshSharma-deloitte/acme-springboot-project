1300e1e68da23a6465837e65925c6d095d46fd1b
yogeshSharma-deloitte <yogeshsharma8@deloitte.com>
2023-09-27 16:49:50 +0000

    Pushed spring boot code converted from EJB

---
+++ src/main/java/com/hashedin/migration/repository/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.hashedin.migration.repository;
+
+import com.hashedin.migration.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findAll();
+
+}
